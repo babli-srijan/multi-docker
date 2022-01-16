1008c96f87776f681804253a55367d481dfa220f
babli-srijan <babli.sah@srijan.net>
2022-01-16 22:23:23 +0530

    test deployment

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            my app is running
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
